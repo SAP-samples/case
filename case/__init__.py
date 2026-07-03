4987927b91078a6f8aaf7d9b12b131e221ac62f7
guentherschindler <guenther.schindler@sap.com>
2026-07-03 07:27:03 +0200

    Update case/__init__.py
    Co-authored-by: Maximilian Schambach <28300359+MaxSchambach@users.noreply.github.com>

--- case/__init__.py
+++ case/__init__.py
@@ -1,4 +1,4 @@
-from .embedding import CaseTransformer
-from .config import SerializationConfig
+from case.embedding import CaseTransformer
+from case.config import SerializationConfig
 
 __all__ = ["CaseTransformer", "SerializationConfig"]
